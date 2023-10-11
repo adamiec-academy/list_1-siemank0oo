3d6f565cfe9b04b40f8265e8bcc142e0f03bdb4a
siemank0oo <105595089+siemank0oo@users.noreply.github.com>
2023-10-11 21:47:23 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,10 +1,10 @@
 def cross(n):
-       for i in range(n):
+    for i in range(n):
         print(' ' * n + '*' * n + ' ' * n)
     for i in range(n):
         print('*' * 3 * n)
     for i in range(n):
         print(' ' * n + '*' * n + ' ' * n)
-    pass
+
 
         
